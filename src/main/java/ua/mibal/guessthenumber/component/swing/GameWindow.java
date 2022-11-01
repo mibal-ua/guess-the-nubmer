91506c1835097bbe6d69f1ed94432c12cffd1a95
mibal_ua <9mohapx9@gmail.com>
2022-11-01 11:49:55 +0100

    Implement GameWindow class

--- src/main/java/ua/mibal/guessthenumber/component/swing/GameWindow.java
+++ src/main/java/ua/mibal/guessthenumber/component/swing/GameWindow.java
@@ -20,33 +20,97 @@ import ua.mibal.guessthenumber.component.DataPrinter;
 import ua.mibal.guessthenumber.component.GameOverHandler;
 import ua.mibal.guessthenumber.component.UserInputReader;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 /**
  * @author Michael Balakhon
  * @link t.me/mibal_ua.
  */
-public class GameWindow implements DataPrinter, UserInputReader, GameOverHandler {
+public class GameWindow extends JFrame implements DataPrinter, UserInputReader, GameOverHandler {
+
+    private static final int FONT_SIZE = 35;
+
+    private static final int CELL_SIZE = 75;
+
+    private int clickedNumber;
+
+    public GameWindow() {
+        super("Guess the number");
+        setSystemLookAndFeel();
+        setLayout(new GridLayout(2, 5));
+        createGameTable();
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+        pack();
+        displayInTheMiddleOfTheScreen();
+    }
+
+    private void setSystemLookAndFeel() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (final ClassNotFoundException | UnsupportedLookAndFeelException |
+                       IllegalAccessException | InstantiationException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void createGameTable() {
+        for (int i = 0; i < 10; i++) {
+            final JLabel label = new JLabel();
+            label.setPreferredSize(new Dimension(CELL_SIZE, CELL_SIZE));
+            label.setHorizontalAlignment(SwingConstants.CENTER);
+            label.setVerticalAlignment(SwingConstants.CENTER);
+            label.setFont(new Font(Font.SERIF, Font.PLAIN, FONT_SIZE));
+            label.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+            label.setText(String.valueOf(i));
+            add(label);
+            final int number = i;
+            label.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(final MouseEvent unused) {
+                    synchronized (GameWindow.this) {
+                        clickedNumber = number;
+                        GameWindow.this.notifyAll();
+                    }
+                }
+            });
+        }
+    }
+
+    private void displayInTheMiddleOfTheScreen() {
+        final Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation(dim.width / 2 - getSize().width / 2, dim.height / 2 - getSize().height / 2);
+        setVisible(true);
+    }
+
     @Override
     public void printInstructions() {
-
+        JOptionPane.showMessageDialog(this, "Select your number", "Info", JOptionPane.INFORMATION_MESSAGE);
     }
 
     @Override
     public void printInfoMessage(final String message) {
-
-    }
-
-    @Override
-    public void printErrorMessage(final String message) {
-
+        JOptionPane.showMessageDialog(this, message, "Info", JOptionPane.INFORMATION_MESSAGE);
     }
 
     @Override
     public int getUserInput() {
-        return 0;
+        synchronized (this) {
+            try {
+                wait();
+            } catch (final InterruptedException exception) {
+                exception.printStackTrace();
+                System.exit(2);
+            }
+        }
+        return clickedNumber;
     }
 
     @Override
     public void gameOver() {
-
+        System.exit(0);
     }
 }
